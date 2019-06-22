21eb9e9131bbadee8d39e9beb0f38f4ef3efc889
Pallav Raj <42534938+plav01@users.noreply.github.com>
2019-06-22 10:00:33 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -33,8 +33,8 @@ session_start();
 
 require_once 'Facebook/autoload.php';
 $fb = new Facebook\Facebook([
-  'app_id' => '648384022292884', // Replace {app-id} with your app id
-  'app_secret' => '5caa6a9e744c2dff3bcf72f6b684802f',
+  'app_id' => '{Your app-id}', // Replace {app-id} with your app id
+  'app_secret' => '{Your app-secret}',
   'default_graph_version' => 'v3.3',
   ]);
 
